eaafe7d907b61f375bcf46f5d4529831024eeef6
L7R-a <gedylarosa@gmail.com>
2024-06-02 00:43:57 -0400

    More attempts trying to deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from "./LandingPage";
 import Experience from "./Experience";
 import About from "./About";
@@ -15,11 +10,11 @@ import SinglePlayerShooter from "./SinglePlayerShooter";
 import Tanks from "./Tanks";
 import WordGame from "./WordGame";
 import RPG from "./RPG";
-import GameJam from "./GameJam"
+import GameJam from "./GameJam";
 import WebProjects from "./WebProjects";
 import VirtualVogue from "./VirtualVogue";
 import ContactManager from "./ContactManager";
-import PortfolioApp from "./PortfolioApp"
+import PortfolioApp from "./PortfolioApp";
 import VRProjects from "./VRProjects";
 import OtherProjects from "./OtherProjects";
 import GraphProblem from "./GraphProblem";
@@ -38,24 +33,23 @@ function App() {
         <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Projects" element={<Projects />} />
-        <Route path="/GameProjects" element={<GameProjects/>} />
-        <Route path="/OnlineMultiplayer" element={<OnlineMultiplayer/>} />
-        <Route path="/SinglePlayerShooter" element={<SinglePlayerShooter/>} />
-        <Route path="/Tanks" element={<Tanks/>} />        
-        <Route path="/WordGame" element={<WordGame/>} />
-        <Route path="/RPG" element={<RPG/>} />
-        <Route path="/GameJam" element={<GameJam/>} />
-        <Route path="/VRProjects" element={<VRProjects/>} />
-        <Route path="/WebProjects" element={<WebProjects/>} />
-        <Route path="/VirtualVogue" element={<VirtualVogue/>} />
-        <Route path="/ContactManager" element={<ContactManager/>} />
-        <Route path="/PortfolioApp" element={<PortfolioApp/>} />
-        <Route path="/Capstone" element={<Capstone/>} />
-        <Route path="/OtherProjects" element={<OtherProjects/>} />
-        <Route path="/GraphProblem" element={<GraphProblem/>} />
-        <Route path="/VM" element={<VM/>} />
-        <Route path="/Schedules" element={<Schedules/>} />
-
+        <Route path="/GameProjects" element={<GameProjects />} />
+        <Route path="/OnlineMultiplayer" element={<OnlineMultiplayer />} />
+        <Route path="/SinglePlayerShooter" element={<SinglePlayerShooter />} />
+        <Route path="/Tanks" element={<Tanks />} />
+        <Route path="/WordGame" element={<WordGame />} />
+        <Route path="/RPG" element={<RPG />} />
+        <Route path="/GameJam" element={<GameJam />} />
+        <Route path="/VRProjects" element={<VRProjects />} />
+        <Route path="/WebProjects" element={<WebProjects />} />
+        <Route path="/VirtualVogue" element={<VirtualVogue />} />
+        <Route path="/ContactManager" element={<ContactManager />} />
+        <Route path="/PortfolioApp" element={<PortfolioApp />} />
+        <Route path="/Capstone" element={<Capstone />} />
+        <Route path="/OtherProjects" element={<OtherProjects />} />
+        <Route path="/GraphProblem" element={<GraphProblem />} />
+        <Route path="/VM" element={<VM />} />
+        <Route path="/Schedules" element={<Schedules />} />
       </Routes>
     </Router>
   );
